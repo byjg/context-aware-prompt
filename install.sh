e9cfdd5d44049477a65e8251c9e4c22e13d5a039
Joao Gilberto Magalhaes <joao@byjg.com.br>
2021-02-01 02:09:50 -0600

    Adjusts in the install script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo
 if [ ! -d "$REPODIR" ]
 then
     cd "$BASEDIR"
-    git clone git://github.com/byjg/git-aware-prompt.git
+    git clone https://github.com/byjg/git-aware-prompt.git
 else
     cd "$REPODIR"
     echo "Updating local copy."
@@ -26,12 +26,17 @@ else
     echo
     echo Installing to $PROFILE
     echo ''  >> $PROFILE
-    echo '# /-- Bash Git: Start'  >> $PROFILE
+    echo '# /-- Context Aware Prompt: Start'  >> $PROFILE
     echo "alias ls='ls --color=always'" >> $PROFILE
     echo "export GITAWAREPROMPT=$REPODIR" >> $PROFILE
     echo 'source $GITAWAREPROMPT/main.sh' >> $PROFILE
-    echo 'export PS1="\u@\h \w \[$txtylw\]\$k8s_config\[$txtrst\]\[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "' >> $PROFILE
-    echo '# \-- Bash Git: End'  >> $PROFILE
+    echo 'export PS1_RESET="$PS1"' >> $PROFILE
+    echo 'export PS1_GIT="\[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]"' >> $PROFILE
+    echo 'export PS1_K8S="\[$txtylw\]\$k8s_config\[$txtrst\]"' >> $PROFILE
+    echo 'export PS1_ALL="$PS1_K8S$PS1_GIT"' >> $PROFILE
+    echo 'export PS1="\u@\h \w $PS1_ALL\$ "' >> $PROFILE
+    echo '# \-- Context Aware Prompt: End' >> $PROFILE
+    echo ''  >> $PROFILE
 fi
 echo
 echo "Done. Please open a new terminal to see the prompt."
