cd32fe5d2ca2753997a964e4fb1c57b7ce6afd67
Joao Gilberto Magalhaes <joao@byjg.com.br>
2021-04-16 00:57:56 -0500

    Fix install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ else
     echo '##Start Context Aware Prompt'  >> $PROFILE
     echo "alias ls='ls --color=always'" >> $PROFILE
     echo "export CONTEXT_AWARE_PROMPT=$REPODIR" >> $PROFILE
-    echo "source $CONTEXT_AWARE_PROMPT/main.sh" >> $PROFILE
+    echo "source \$CONTEXT_AWARE_PROMPT/main.sh" >> $PROFILE
     echo 'export PS1="\[$txtylw\]\$context_k8s\[$txtrst\]\n\u@\h \w \[$txtcyn\]\$context_git_branch\[$txtred\]\$context_git_dirty\[$txtrst\]\$ "' >> $PROFILE
     echo '##End Context Aware Prompt' >> $PROFILE
     echo ''  >> $PROFILE
